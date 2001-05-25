b0cffc2dc828c7219eb3d95b8093748b585d02df
Sean M. Burke <sburke@cpan.org>
2001-05-25 06:21:01 -0800

    import Locale-Maketext 1.01 from CPAN
    git-cpan-module:   Locale-Maketext
    git-cpan-version:  1.01
    git-cpan-authorid: SBURKE
    git-cpan-file:     authors/id/S/SB/SBURKE/Locale-Maketext-1.01.tar.gz

--- test.pl
+++ test.pl
@@ -1,14 +1,39 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
-# Time-stamp: "1999-03-06 15:59:05 MST"
+# Time-stamp: "2001-05-25 08:07:12 MDT"
 ######################### We start with some black magic to print on failure.
 
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..1\n"; }
-END {print "BAD! 1\n" unless $loaded;}
-use Locale::Maketext;
+BEGIN { $| = 1; print "1..3\n"; }
+END {print "fail 1\n" unless $loaded;}
+use Locale::Maketext 1.01;
+print "Perl v$], Locale::Maketext v$Locale::Maketext::VERSION\n";
 $loaded = 1;
-print "OK 1\n";
+print "ok 1\n";
+{
+  package Woozle;
+  @ISA = ('Locale::Maketext');
+  sub dubbil { return $_[1] * 2 }
+}
+{
+  package Woozle::elx;
+  @ISA = ('Woozle');
+  %Lexicon = (
+   'd2' => 'hum [dubbil,_1]',
+  );
+}
 
-######################### End of black magic.
+$lh = Woozle->get_handle('elx');
+if($lh) {
+  print "ok 2\n";
+  my $x = $lh->maketext('d2', 7);
+  if($x eq "hum 14") {
+    print "ok 3\n";
+  } else {
+    print "fail 3\n  (got \"$x\")\n";
+  }
+} else {
+  print "fail 2\n";
+}
+#Shazam!
