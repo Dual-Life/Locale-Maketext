c81410ad5119d5cfe888046160a09f8d0ee9407d
Andy Lester <andy@petdance.com>
2005-11-10 19:42:57 -0800

    import Locale-Maketext 1.10 from CPAN
    git-cpan-module:   Locale-Maketext
    git-cpan-version:  1.10
    git-cpan-authorid: PETDANCE
    git-cpan-file:     authors/id/P/PE/PETDANCE/Locale-Maketext-1.10.tar.gz

--- lib/Locale/Maketext.pm
+++ lib/Locale/Maketext.pm
@@ -14,7 +14,7 @@ use I18N::LangTags 0.30 ();
 BEGIN { unless(defined &DEBUG) { *DEBUG = sub () {0} } }
  # define the constant 'DEBUG' at compile-time
 
-$VERSION = "1.09";
+$VERSION = "1.10";
 @ISA = ();
 
 $MATCH_SUPERS = 1;
